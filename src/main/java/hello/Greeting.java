e2a55084156ecfbf99be02ed7598aa36606c85bd
Oleh Krupenia <oleh.krupenia@globallogic.com>
2015-10-13 21:47:22 -0400

    adding of the test websocket functionality

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ * Created by oleh.krupenia on 10/13/2015.
+ */
+public class Greeting {
+    private String content;
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by oleh.krupenia on 10/13/2015.
+ */
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(3000); // simulated delay
+        return new Greeting("Hello, " + message.getName() + "!");
+    }
+
+}
